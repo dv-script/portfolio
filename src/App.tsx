1b48f800605274296376bcbe48d503550bb5f949
Davi Hasson Castro <davihasson@gmail.com>
2023-06-02 18:34:18 -0300

    chore: routing pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
+import RouteComponent from './routes'
+import { GlobalStyle } from './GlobalStyle'
+
 export default function App() {
   return (
     <>
+      <GlobalStyle />
+      <RouteComponent />
     </>
   )
 }
