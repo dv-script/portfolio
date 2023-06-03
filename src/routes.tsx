adc9bc4e747a1fe71847f54f8d52793f3c348c60
Davi Hasson Castro <davihasson@gmail.com>
2023-06-03 19:51:04 -0300

    chore: adding Skills Page

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import Home from './pages/Home'
 import About from './pages/About'
+import Skills from './pages/Skills'
 import Projects from './pages/Projects'
 import Contact from './pages/Contact'
 
@@ -11,6 +12,7 @@ export default function RouteComponent() {
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/about' element={<About></About>}></Route>
+        <Route path='/skills' element={<Skills></Skills>}></Route>
         <Route path='/projects' element={<Projects></Projects>}></Route>
         <Route path='/contact' element={<Contact></Contact>}></Route>
       </Routes>
